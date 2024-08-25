ca0702d97c8872d7ccc86663214f0c6341380b39
Pablo-fp <pablofp1988@hotmail.com>
2024-08-25 23:02:59 +0200

    4.1 Blog List, step 1

--- blog_backend/index.js
+++ blog_backend/index.js
@@ -1,42 +1,54 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+require('dotenv').config().parsed;
 
-// const blogSchema = new mongoose.Schema({
-//   title: String,
-//   author: String,
-//   url: String,
-//   likes: Number
-// })
+const cors = require('cors');
+const mongoose = require('mongoose');
 
-// const Blog = mongoose.model('Blog', blogSchema)
+const express = require('express');
+const app = express();
 
-// const mongoUrl = 'mongodb://localhost/bloglist'
-// mongoose.connect(mongoUrl)
+const mongoUrl = process.env.MONGODB_URL;
+mongoose.connect(mongoUrl);
 
-app.use(cors())
-app.use(express.json())
+mongoose
+  .connect(mongoUrl)
+  .then((result) => {
+    console.log('connected to MongoDB');
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message);
+  });
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+});
+
+const Blog = mongoose.model('Blog', blogSchema);
+
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
+  response.send('<h1>Hello World!</h1>');
+});
 
-// app.get('/api/blogs', (request, response) => {
-//   Blog.find({}).then((blogs) => {
-//     response.json(blogs)
-//   })
-// })
+app.get('/api/blogs', (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs);
+  });
+});
 
-// app.post('/api/blogs', (request, response) => {
-//   const blog = new Blog(request.body)
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body);
 
-//   blog.save().then((result) => {
-//     response.status(201).json(result)
-//   })
-// })
+  blog.save().then((result) => {
+    response.status(201).json(result);
+  });
+});
 
-const PORT = 3003
+const PORT = 3003;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
