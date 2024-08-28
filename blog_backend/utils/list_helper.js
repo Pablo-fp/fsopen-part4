7384f26405a096c945893cf9715d67f52bc8db91
Pablo-fp <pablofp1988@hotmail.com>
2024-08-28 23:01:59 +0200

    4.4: Helper Functions and Unit Tests, step 2

--- blog_backend/utils/list_helper.js
+++ blog_backend/utils/list_helper.js
@@ -2,6 +2,14 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  if (blogs.lenght === 0) return 0;
+  if (blogs.lenght === 1) return blog.likes;
+  const reducer = (sum, blog) => sum + blog.likes;
+  return blogs.reduce(reducer, 0);
+};
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 };
