310a01a243d7f35e34db1c905b97fba0fc2d9aea
Pablo-fp <pablofp1988@hotmail.com>
2024-09-08 20:38:50 +0200

    Initialized the database before tests

--- blog_backend/tests/blog_api.test.js
+++ blog_backend/tests/blog_api.test.js
@@ -1,9 +1,34 @@
-const { test, after } = require('node:test');
+const { test, after, beforeEach } = require('node:test');
+const assert = require('node:assert');
+const Blog = require('../models/blog');
 const mongoose = require('mongoose');
 const supertest = require('supertest');
 const app = require('../app');
 
 const api = supertest(app);
+
+const initialBlogs = [
+  {
+    title: 'HTML is easy',
+    author: 'Jonhy',
+    url: 'www.jonhy.com',
+    likes: 5
+  },
+  {
+    title: 'Browser can execute only JavaScript',
+    author: 'Carla',
+    url: 'www.carla.com',
+    likes: 11
+  }
+];
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+  let blogObject = new Blog(initialBlogs[0]);
+  await blogObject.save();
+  blogObject = new Blog(initialBlogs[1]);
+  await blogObject.save();
+});
 
 test('blogs are returned as json', async () => {
   await api
@@ -15,15 +40,15 @@ test('blogs are returned as json', async () => {
 test('there are two blogs', async () => {
   const response = await api.get('/api/blogs');
 
-  assert.strictEqual(response.body.length, 2);
+  assert.strictEqual(response.body.length, initialNotes.length);
 });
 
 test('the first blog is about HTTP methods', async () => {
   const response = await api.get('/api/blogs');
 
-  const contents = response.body.map((e) => e.content);
+  const titles = response.body.map((e) => e.title);
   // is the argument truthy
-  assert(contents.includes('HTML is easy'));
+  assert(titles.includes('HTML is easy'));
 });
 
 after(async () => {
